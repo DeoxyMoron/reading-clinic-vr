00afcd8ba0b6e7c4f93ed7ee916e413d7e8f6db0
David <dsanosa@gmail.com>
2019-04-24 18:46:32 -0700

    added audio manager

---
+++ main-unity-project/dev-main-project/Assets/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<AudioManager>().Play("Bell");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            print("space");
+            FindObjectOfType<AudioManager>().Play("Bell");
+        }
+    }
+}
